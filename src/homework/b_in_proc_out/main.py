c97d410807371affe56c0cb659401031283d73d6
juliancotto <julian.cotto@g.austincc.edu>
2022-02-09 20:13:54 -0600

    Homework 1 was hard

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,10 @@
-#
+from output import multiply_numbers
+
+#num1 = int(input())
+#num2 = int(input())
+
+result = multiply_numbers(5 , 5)
+result = multiply_numbers(10 , 10)
+print(result)
+
+print("end")
