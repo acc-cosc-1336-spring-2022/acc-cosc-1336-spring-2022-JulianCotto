c897ecdd0efa3a29050cc624196b47367349a3a6
juliancotto <julian.cotto@g.austincc.edu>
2022-03-05 20:56:04 -0600

    commit

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,8 @@
 import unittest
 
 
-from tests.homework.d_repetition import tests_repetition
+from tests.homework.e_functions import tests_functions
 
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_repetition)
+suite = unittest.TestLoader().loadTestsFromModule(tests_functions)
 unittest.TextTestRunner(verbosity=2).run(suite)
