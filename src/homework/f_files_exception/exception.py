e252e641398b560ce64b89b7c9a3a0d6f7a6ad1e
JulianCotto <98437823+JulianCotto@users.noreply.github.com>
2022-04-23 17:13:50 -0500

    Update exception.py

--- src/homework/f_files_exception/exception.py
+++ src/homework/f_files_exception/exception.py
@@ -1,1 +1,17 @@
-#
+def add_inventory(dictionary, widget_name, quantity):
+    if widget_name not in dictionary:
+        dictionary[widget_name] = quantity
+        widget_file = open("widgets.txt", "w")
+        widget_file.write(str(dictionary))
+        widget_file.close()
+    else:
+        dictionary[widget_name] += quantity
+        widget_file = open("widgets.txt", "w")
+        widget_file.write(str(dictionary))
+        widget_file.close()
+    return dictionary
+    
+def display_items(widget_file):
+    widget_file = open("widgets.txt", "r")
+    file_contents = widget_file.read()
+    return file_contents
