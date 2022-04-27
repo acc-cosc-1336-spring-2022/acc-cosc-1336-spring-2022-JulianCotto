54f18f18614ec8f98e170d5fd3e46a5e510e2a7d
JulianCotto <98437823+JulianCotto@users.noreply.github.com>
2022-04-26 22:42:52 -0500

    Update class_a.py

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,27 @@
-#
+import random
+
+class Die:
+
+    def roll(self):
+        __roll_value = random.randint(1, 6)
+        return __roll_value
+
+    def get_roll_value(self):
+        return self.roll()
+
+    def __str__(self):
+        return str("The rolled value is {0}".format(self.get_roll_value()))
+
+def main():
+    decision = "n"
+    die = Die()
+    decision = input("Do you want to roll the Die? y or n?")
+    while decision.lower() == "y":
+        print(die)
+        decision = input("Do you want to roll the Die again? y or n?")
+    while decision.lower() == "n":
+        print("Thanks for playing!!")
+        quit()
+    else:
+        print("Invalid Input. Enter y or n")
+        main()
