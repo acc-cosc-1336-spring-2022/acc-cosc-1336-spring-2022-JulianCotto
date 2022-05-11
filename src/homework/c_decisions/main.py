0b78b9a2cbd147b66f3c09f6845e66a79c8cd3d0
JulianCotto <98437823+JulianCotto@users.noreply.github.com>
2022-05-10 22:43:25 -0500

    Update main.py

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,29 +1,28 @@
-grade_list = []
-total = 0
-decision1 = "y"
-while decision1 == "y" or decision1 == "Y":
-    st_grade = int(input("Please enter student grade"))
-    grade_list.append(st_grade)
-    if st_grade >= 90 and st_grade <= 100:
-        print("Student number", len(grade_list), ":", "You got an A!! Why dont you teach the next class!!")
-    elif st_grade >= 80 and st_grade < 90:
-        print("Student number", len(grade_list), ":", "You got an B!! There is a Little More Practice Required!!")
-    elif st_grade >= 70 and st_grade < 80:
-        print("Student number", len(grade_list), ":", "You got an C!! Practice your fundamentals more!!")
-    elif st_grade >= 60 and st_grade < 70:
-        print("Student number", len(grade_list), ":", "You got an D!! Are you even trying?!")
-    elif st_grade < 60 and st_grade >= 0:
-        print("Student number", len(grade_list), ":", "You got an F!! This is an expensive class to retake!!")
-    else: 
-        print("Invalid Input")
-    decision1 = input("Do you want to check another grade? y/n")
-for value in grade_list:
-    total += int(value)
-    average = total / len(grade_list)
-print("The number of grades you entered is:", len(grade_list))
-print("The average grade of the class is:", average)
+import decisions
 
 
+def main():
+    run_again = "y"
+    while run_again.lower() == "y":
+        total = input("Please enter the amount of points available on your assignment")
+        options = input("Please enter how many points you earned on your assignment")
+        if int(options) < 0 or int(options) > int(total):
+            print("Invalid Entry. You cannot have a grade less than 0 or more than the total available points")
+            main()
+        print("Now calculating your grade")
+        grade = decisions.get_options_ratio(int(options), int(total))
+        fac_grade = decisions.get_faculty_rating(grade)
+        if grade >= .6 and grade < .7:
+            print("You got a grade of", grade*100, "% which", fac_grade)
+            run_again = input("Would you like to enter more grades? Y or N")
+        else:
+            print("You got a grade of", grade*100, "% which is", fac_grade)
+            run_again = input("Would you like to enter more grades? Y or N")
+    else:
+        print("Thank you for running this grading program")
+        quit()
+
+main()
 
 
 
