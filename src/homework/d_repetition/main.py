9ca01999e50c33f68e5b56af15dd2f42ab557e3a
JulianCotto <98437823+JulianCotto@users.noreply.github.com>
2022-05-10 23:07:40 -0500

    Update main.py

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,30 +1,37 @@
-from tkinter.messagebox import YES
 import repetition
 
+def main():
+    dec = input("Homework 3 Menu\nPlease select a menu option\n1-Factorial\n2-Sum Odd Numbers\n3-Exit")
+    if dec == "1":
+        num = input("Please enter a number greater than 0 and less than 10")
+        while int(num) < 0 or int(num) > 10:
+            print("Invalid Entry")
+            num = input("Please enter a number greater than 0 and less than 10")
+        fact = repetition.get_factorial(int(num))
+        print(fact)
+        dec0 = input("Do you want to quit? Y or N")
+        if dec0.lower() == "y":
+            print("Thank you for running this math program")
+            quit()
+        else:
+            main()
 
-print("Homework 3 Menu\nPlease select a menu option\n1-Factorial\n2-Sum Odd Numbers\n3-exit")
+    elif dec == "2":
+        num1 = input("Please enter a number greater than 0 and less than 100")
+        while int(num1) < 0 or int(num1) > 100:
+            print("Invalid Entry")
+            num1 = input("Please enter a number greater than 0 and less than 100")
+        odd_sum = repetition.sum_odd_numbers(int(num1))
+        print(odd_sum)
+        dec1 = input("Do you want to quit? Y or N")
+        if dec1.lower() == "y":
+            print("Thank you for running this math program")
+            quit()
+        else:
+            main()
 
+    elif dec == "3":
+        print("Goodbye")
+        quit()
 
-
-if
-    num = int(input("Please enter a number between 1 and 10"))
-    if num > 0 and num < 10:
-        factorial = repetition.get_factorial(num)
-        print(factorial)
-    else:   
-        print("Invalid Entry")
-        num = int(input("Please enter a number between 1 and 10"))
-    print(repetition.get_factorial(num))
-
-elif choice == 2:
-    num = int(input("Please enter a number  greater than 0 and less than 100"))
-    if num > 0 and num < 100:
-        sums = repetition.sum_odd_numbers(num)
-        print(sums)
-    else: 
-        print("Invalid Entry")
-        num = int(input("Please enter a number  greater than 0 and less than 100"))
-    print(repetition.sum_odd_numbers(num))
-    
-
-
+main()
