82daa73a554a66965a91a6f4705e1d7177c3f068
JulianCotto <98437823+JulianCotto@users.noreply.github.com>
2022-05-10 22:42:18 -0500

    Update decisions.py

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,3 +1,24 @@
+def get_options_ratio(option, total):
+    value = option/total
+    return value
 
+def get_faculty_rating(value):
+    if value == 1:
+        rating = "Perfect!!"
+    elif value >= .9 and value < 1:
+        rating = "Excellent"
+    elif value >= .8 and value < .9:
+        rating = "Very Good"  
+    elif value >= .7 and value < .8:
+        rating = "Good" 
+    elif value >= .6 and value < .7:
+        rating = "Needs Improvement" 
+    elif value >= 0 and value <= .59:
+        rating = "Unacceptable"
+    else:
+        print("Invalid Entry!")
+    return rating
+
+ 
 
     
